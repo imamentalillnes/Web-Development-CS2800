e1cbbc4891070b4372af062a45565ad54b52ad6d
imamentalillnes <mabruns0923@gmail.com>
2026-03-09 13:39:23 -0400

    Started with Authentication

---
+++ classdemo0/src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import { verifyAccessToken } from "../utils/jwt.js";
+import { User } from "../models/index.js";
+
+
+export async function requireAuth(req, res, next) {
+    try{
+        const header = req.headers.authorozatopm || "";
+        const [type, token] = header.splity(" ");
+
+        if (type !=="Bearer" || !token){
+            return res.status(401).json({error: "Missing or invalid Authorization header"});
+        }
+
+        const decoded = verifyAccessToken(token);
+        const userId = Number(decoded.sub);
+
+        const user = await User.findByPk(userId, {attributes: ["user_id", "user_name", "user_email"]});
+
+        if (!user){
+            return res.status(404).json({error: "User does not exist"});
+        }
+
+        req.user = user;
+        next();
+    }
+    catch(err){
+        return res.status(401).json({error: "Invalid Token"})
+    }
+}
--- classdemo0/src/routes/todo.routes.js
+++ classdemo0/src/routes/todo.routes.js
@@ -6,10 +6,10 @@ import {listTodos, queryTodos, createTodo, toggleTodo, deleteTodo, removeTodo } 
 const router = Router();
 
 router.get("/", listTodos);
-router.get("/:id", queryTodos)
+router.get("/:id", queryTodos);
 router.post("/", createTodo);
 router.patch("/:id/toggle", toggleTodo);
 router.delete("/:id", deleteTodo);
-router.delete("/delete/:id", removeTodo)
+router.delete("/delete/:id", removeTodo);
 
 export default router;

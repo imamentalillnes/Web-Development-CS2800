509a6e755212c978e62e302a156d39354119b208
imamentalillnes <mabruns0923@gmail.com>
2026-03-09 14:16:56 -0400

    Created auth.service.js Started working on the creating of users, hashing of passwords, and the logining in of already made users.

---
+++ classdemo0/src/services/auth.service.js
@@ -0,0 +1,46 @@
+import bycrypt from "bycrypt";
+import { User } from "../models/index.js"
+import { signAccessToken } from "../utils/jwt.js"
+
+const SALT_ROUNDS = 10;
+
+export async function register({name, email, password}) {
+    const normalizeEmail = email.toLowerCase();
+
+    const existing = await User.findOne({where: {user_email: normalizeEmail}});
+    if (existing){
+        return {ok: false, status: 409, error:"Email already registered"};
+    }
+
+    const passwordHash = await bycrypt.hash(password, SALT_ROUNDS);
+
+    const user = await User.create({
+        name,
+        user_email: normalizeEmail,
+        passwordHash
+    });
+
+    const token = signAccessToken( {sub: String(user.user_id), email: user_email})
+
+    return {ok:true, data:{token, user:{id: user.user_id, name: user.user_email}}}
+
+
+}
+
+export async function login({email, password}) {
+    const normalizeEmail = email.toLowerCase();
+
+    const user = await User.findOne({where: {user_email: normalizeEmail}});
+
+    if(!(user)){
+        return {ok: false, status: 401, error:"Invalid Credentials"};
+    }
+    
+    const match = await bycrypt.compare(password, user.passwordHash);
+    if(!match){
+        return {ok: false, status: 401, error:"Invalid Credentials"};
+    }
+
+    const token = signAccessToken({sub: String(user.user_id), email: user.user_email});
+    return {ok : true, data: {token, user:{id:user.user_id, name: user_name, email: user_email}}};
+}
